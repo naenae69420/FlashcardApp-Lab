f19b07c1aac845ae434cfff526cefea6f99399b6
Nathan Le <lenathanjon@gmail.com>
2022-10-01 20:58:27 -0700

    Edited readme files and added other files from lab 2

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,35 +1,86 @@
 package com.example.flashcardnewapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.flashcard_question).setOnClickListener {
-            findViewById<View>(R.id.flashcard_question).visibility = View.INVISIBLE
-            findViewById<View>(R.id.flashcard_answer).visibility = View.VISIBLE
+
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+
+        flashcardQuestion.setOnClickListener {
+            flashcardAnswer.visibility = View.VISIBLE
+            flashcardQuestion.visibility = View.INVISIBLE
         }
-        findViewById<View>(R.id.flashcard_answer).setOnClickListener {
-            findViewById<View>(R.id.flashcard_question).visibility = View.VISIBLE
-            findViewById<View>(R.id.flashcard_answer).visibility = View.INVISIBLE
+        flashcardAnswer.setOnClickListener {
+            flashcardAnswer.visibility = View.INVISIBLE
+            flashcardQuestion.visibility = View.VISIBLE
         }
+
         findViewById<View>(R.id.flashcard_answertwo).setOnClickListener {
-            findViewById<View>(R.id.flashcard_answertwo).setBackgroundColor(getResources().getColor(R.color.my_red_color, null))
+            findViewById<View>(R.id.flashcard_answertwo).setBackgroundColor(
+                getResources().getColor(
+                    R.color.my_red_color,
+                    null
+                )
+            )
         }
         findViewById<View>(R.id.flashcard_answerthree).setOnClickListener {
-            findViewById<View>(R.id.flashcard_answerthree).setBackgroundColor(getResources().getColor(R.color.my_red_color, null))
+            findViewById<View>(R.id.flashcard_answerthree).setBackgroundColor(
+                getResources().getColor(
+                    R.color.my_red_color,
+                    null
+                )
+            )
         }
         findViewById<View>(R.id.flashcard_answerfour).setOnClickListener {
-            findViewById<View>(R.id.flashcard_answerfour).setBackgroundColor(getResources().getColor(R.color.my_green_color, null))
+            findViewById<View>(R.id.flashcard_answerfour).setBackgroundColor(
+                getResources().getColor(
+                    R.color.my_green_color,
+                    null
+                )
+            )
         }
 
+        findViewById<View>(R.id.add_question_button).setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            startActivity(intent)
+        }
 
+        val resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
 
+                val data: Intent? = result.data
+                if (data != null) {
+                    val questionString = data.getStringExtra("QUESTION_KEY")
+                    val answerString = data.getStringExtra("ANSWER_KEY")
 
+                    flashcardQuestion.text = questionString
+                    flashcardAnswer.text = answerString
+
+                    Log.i("MainActivity", "question: $questionString")
+                    Log.i("=MainActivity", "answer: $answerString")
+                } else {
+                    Log.i("=MainActivity", "Returned null data from AddCardActivity")
+                }
+            }
+
+        val addQuestionButton = findViewById<ImageView>(R.id.add_question_button)
+        addQuestionButton.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            resultLauncher.launch(intent)
+        }
 
 
     }
--- README.md
+++ README.md
@@ -1,7 +1,31 @@
 ## FlashcardApp
 
-📝 `NOTE:` Paste the README templates for each subsequent lab here at the top, (i.e. lab 2, 3, 4). This will show a history of your development process including which users tories you completed and how your app looked and functioned at each step.
+📝 `NOTE` Paste this template at the top of your existing `README.md` file from the last lab.
 
+## Lab 2
+
+### App Description
+ A new page feature was added which could save existing text. 
+
+### App Walk-though
+
+<img src="http://g.recordit.co/54mBXnA6Lf.gif" width=200><br>
+
+## Required
+- [x] User can click on a ‘+’ button that takes the user to new ‘Add Card Screen’
+- [x] The 'Add Card Screen' has a cancel button to take the user back to the main screen
+- [x] User can enter and submit a Question and Answer through the 'Add Card Screen'
+- [x] User will see the card they just created on the main screen when they press the 'Save Button'
+- [x] Push your progress to GitHub!
+
+## Optional
+- [ ] User can edit a card
+- [ ] An error message is shown if the user doesn't enter both a Question and an Answer
+- [ ] A notification is shown if the card was created successfully
+- [ ] User can also enter multiple choice answers when creating a card
+- [ ] App is further styled and customized!
+
+----------------------------------------------------------------------------------------------------------------------
 ## Lab 1
 
 ### App Description
@@ -19,7 +43,7 @@ I created a flashcard app that asks a question and tells you the answer after cl
 - [x] Add a view for the front side of the flashcard to display the question
 - [x] Add a view for the back side of the flashcard to display the answer
 - [x] Build in logic to show the answer side when the card is tapped
-- [x] Push code to GitHu
+- [x] Push code to GitHub
 ## Optional
 - [x] toggle the flashcard between the question side and the answer side
 - [x] Style the question and answer side of the card to better distinguish between the two sides
